91049977d547c583adefaffc21ac5e1cc751f2e8
yuritanu <fool0628@icloud.com>
2020-06-30 22:48:54 +0900

    mk7

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,8 @@ class PhotosController < ApplicationController
   def index
     
   end
+
+  def new
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'tops#index'
 
-  resources :tops, only: [:index, :show]
-  resources :photos, only: [:index]
+  resources :tops, only: [:index, :show] do
+    get 'myp'
+  end
+  resources :photos, only: [:index, :new]
 
   # カテゴリー関係
   resources :categories, only: [:new]
