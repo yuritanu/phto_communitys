de50990d617f2cca0d85a69ecb6e1fc7ea0d429e
yuritanu <fool0628@icloud.com>
2020-07-03 23:43:52 +0900

    ctegori

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_140026) do
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "ancestry"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
-
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "comment", null: false
-    t.bigint "user_id", null: false
-    t.bigint "photo_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["photo_id"], name: "index_comments_on_photo_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
-    t.string "name", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_photos_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_06_13_134751) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -53,7 +26,4 @@ ActiveRecord::Schema.define(version: 2020_06_13_140026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "photos"
-  add_foreign_key "comments", "users"
-  add_foreign_key "photos", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 # 親の設定
-1 = Categorie.create(name: "")
-2 = Categorie.create(name: "")
-3 = Categorie.create(name: "")
+animal = Categorie.create(name: "動物")
+book = Categorie.create(name: "本")
+entertainer = Categorie.create(name: "芸能")
+landscape = Categorie.create(name: "風景")
+fashion = Categorie.create(name: "ファッション")
+other = Categorie.create(name: "その他")
 
 # 子の設定
-1_1 = 1.children.create(name: "")
-1_2 = 1.children.create(name: "")
-1_3 = 1.children.create(name: "")
+animal_cat = animal.children.create(name: "猫")
+animal_dog = animal.children.create(name: "犬")
+animal_bird = animal.children.create(name: "鳥")
+animal_fish = animal.children.create(name: "魚")
+animal_other = animal.children.create(name: "その他")
 
-2_1 = 2.children.create(name: "")
-2_2 = 2.children.create(name: "")
-2_3 = 2.children.create(name: "")
+book_manga = book.children.create(name: "漫画")
+book_novel = book.children.create(name: "小説")
+book_academic = book.children.create(name: "学術")
+book_other = book.children.create(name: "その他")
 
+entertainer_female = entertainer.children.create(name: "女性")
+entertainer_male = entertainer.children.create(name: "男性")
+entertainer_geinin = entertainer.children.create(name: "芸人")
+entertainer_other = entertainer.children.create(name: "その他")
 
-# 孫の設定
-1_1.children.create([{name: ""}, {name: ""}, {name: ""}])
-1_2.children.create([{name: ""}, {name: ""}, {name: ""},{name: ""}])
-1_3.children.create([{name: ""}, {name:""},{name:""}])
+landscape_nature = landscape.children.create(name: "自然")
+landscape_food = landscape.children.create(name: "食べ物")
+landscape_other = landscape.children.create(name: "その他")
 
-2_1.children.create([{name: ""}, {name:""},{name:""}])
-2_2.children.create([{name: ""}, {name:""},{name:""}])
-2_3.children.create([{name: ""}, {name:""},{name:""}])
+fashion_female = fashion.children.create(name: "女性物")
+fashion_male = fashion.children.create(name: "男性物")
+fashion_cosmetics = fashion.children.create(name: "コスメ")
+fashion_other = fashion.children.create(name: "その他")
+
+other_chat = other.children.create(name: "雑談")
+other_horror = other.children.create(name: "ホラー")
+other_other = other.children.create(name: "その他")
+
+# 孫の設定（今回はなし）
+# 1_1.children.create([{name: ""}, {name: ""}, {name: ""}])
 
